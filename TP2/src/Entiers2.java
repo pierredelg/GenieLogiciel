b34b20263e00cdefaebb54b8a11fd5c6866cc7c7
Delgrange Pierre <pierredelgrange@hotmail.fr>
2019-09-10 18:51:00 +0200

    derniere question modif

--- TP2/src/Entiers2.java
+++ TP2/src/Entiers2.java
@@ -17,12 +17,12 @@ public class Entiers2 {
         Integer a = valeur ;
         int b = valeur ;
         Integer c = valeur ;
-        System.out.println(a == b) ;
-        System.out.println(b == c) ;
+        System.out.println(a == b) ;//On compare un Integer avec un int donc l'unboxing automatique de l'Integer se fait
+        System.out.println(b == c) ;//pareil
         System.out.println(a == c) ;
         //Les Integers sont stockés en mémoire cache de la jvm (dans la classe IntegerCache) dans un tableau pour les valeurs de -128 à +127
-        //Donc l'adresse est la meme pour cet intervalle
-        //En dehors de cet intervalle un nouvel objet est créé donc l'adresse sera différente
+        //L'adresse est la meme pour cet intervalle car l'adresse de l'Integer est trouvé dans le cache
+        //En dehors de cet intervalle un nouvel objet est créé donc l'adresse sera nouvelle et différente
         //explication : https://www.owasp.org/index.php/Java_gotchas
     }
 }
