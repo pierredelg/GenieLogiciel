4a4a8dbb9e458b75de4d1ebe79b13e29c7086145
Delgrange Pierre <pierredelgrange@hotmail.fr>
2019-09-10 18:44:32 +0200

    derniere question

--- TP2/src/Entiers2.java
+++ TP2/src/Entiers2.java
@@ -1,7 +1,16 @@
 public class Entiers2 {
     public static void main(String[] args) {
+        System.out.println("100 : ");
         tester(100) ;
-        System.out.println();
+        System.out.println("\n127 :");
+        tester(127) ;
+        System.out.println("\n128 : ");
+        tester(128) ;
+        System.out.println("\n-128 : ");
+        tester(-128) ;
+        System.out.println("\n-129 : ");
+        tester(-129) ;
+        System.out.println("\n1000 : ");
         tester(1000) ;
     }
     public static void tester(int valeur) {
@@ -11,5 +20,9 @@ public class Entiers2 {
         System.out.println(a == b) ;
         System.out.println(b == c) ;
         System.out.println(a == c) ;
+        //Les Integers sont stockés en mémoire cache de la jvm (dans la classe IntegerCache) dans un tableau pour les valeurs de -128 à +127
+        //Donc l'adresse est la meme pour cet intervalle
+        //En dehors de cet intervalle un nouvel objet est créé donc l'adresse sera différente
+        //explication : https://www.owasp.org/index.php/Java_gotchas
     }
 }
