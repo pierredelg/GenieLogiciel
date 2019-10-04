151bbcf7c2275f1029e9d81ea8a991ec4601352f
Delgrange Pierre <pierredelgrange@hotmail.fr>
2019-10-04 23:19:07 +0200

    generation javadoc

--- Projet3(Tours de Hanoi)/sources/AffichageSimple.java
+++ Projet3(Tours de Hanoi)/sources/AffichageSimple.java
@@ -3,6 +3,12 @@
  */
 public class AffichageSimple implements Affichage {
 
+    /**
+     * Affiche chaque diametre de disques.
+     * @param d - Le tableau de Disque
+     * @param n - Le nombre d'éléments dans le tableau.
+     * @return Une chaine de caractères représentant la pile de Disque.
+     */
     @Override
     public String affichage_tableau(Disque[] d, int n) {
 
