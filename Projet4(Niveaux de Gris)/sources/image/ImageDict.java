a2fbab67ef556a15864a7dbb6f4174278ddf3b48
Delgrange Pierre <pierredelgrange@hotmail.fr>
2019-10-22 23:33:27 +0200

    ajout Projet calculatrice

--- Projet4(Niveaux de Gris)/sources/image/ImageDict.java
+++ Projet4(Niveaux de Gris)/sources/image/ImageDict.java
@@ -5,40 +5,79 @@ import dictionnaire.correction.CoupleObj;
 import dictionnaire.correction.Dictionnaire;
 import dictionnaire.correction.TabDict;
 
+  /*            ********************  REPONSES AUX QUESTIONS  ********************
+
+    Question 4 :
+
+    Cette classe (ImageDict) est plus lente car elle utilise un dictionnaire qui est un objet.
+    Alors que la classe ImageTab utilise un tableau.
+    L'accès au données du tableau est beaucoup plus rapide car il est direct.
+    On ne passe pas par des méthodes pour accéder à son contenu.
+    De plus, avec ImageTab, on utilise des int qui sont des types primitifs et donc l'accès aux données est aussi plus direct.
+    Une solution pour accélérer le temps d'excution dans cette classe serait de
+    remplacer le Couple par la classe Point qui utilise des int et implemente l'interface Couple.
+
+    Question 6 :
+    Avec image.Viewer, si on ajoute l'image du cheval à celle de l'éléphant, on obtient une image représentant un aigle.
+
+    */
+
 /**
  * Classe représentant une image en niveaux de gris avec un Dictionnaire
  * contenant les coordonnées de chaque pixel associées à un niveau de gris.
  */
 public class ImageDict implements ImageGrise {
 
-    /*Réponse question 4 :
-        Cette classe est plus lente car elle utilise un dictionnaire qui est un objet.
-        Alors que la classe ImageTab utilise un tableau.
-        L'accès au données du tableau est beaucoup plus rapide car il est direct.
-        On ne passe pas par des méthodes pour accéder à son contenu.
-        Une solution pour accélérer le temps d'excution dans cette classe serait de
-         remplacer le Couple par la classe Point.
-    */
+    /**
+     *L'objet Dictionnaire associant un Couple de Integer avec un NiveauGris.
+     */
     private Dictionnaire<Couple<Integer,Integer>,NiveauGris> dictionnaire;
+
+    /**
+     * La largeur de l'image.
+     */
     private int largeur;
+
+    /**
+     * La hauteur de l'image.
+     */
     private int hauteur;
 
+    /**
+     * Le constructeur de l'ImageDict. Avec en parametre la largeur et la hauteur de l'image.
+     * @param largeur - La largeur de l'image.
+     * @param hauteur - La hauteur de l'image.
+     */
     public ImageDict(int largeur, int hauteur) {
         this.largeur = largeur;
         this.hauteur = hauteur;
         dictionnaire = new TabDict<>();
     }
 
+    /**
+     * Donne la largeur de l'image.
+     * @return la largeur de l'image.
+     */
     @Override
     public int largeur() {
         return largeur;
     }
 
+    /**
+     * La hauteur de l'image.
+     * @return - La hauteur de l'image.
+     */
     @Override
     public int hauteur() {
         return hauteur;
     }
 
+    /**
+     * Donne le niveau de gris d'une position dans l'image donnée en parametre.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     * @return Le NiveauGris à la position donnée.
+     */
     @Override
     public NiveauGris pointEn(int x, int y) {
         NiveauGris niveauGris = null;
@@ -51,6 +90,13 @@ public class ImageDict implements ImageGrise {
         return niveauGris;
     }
 
+    /**
+     * Définie le niveau de gris (NiveauGris) sur une position dans l'image.
+     * Si le niveau de gris est BLANC on retire le point de l'image dans le dictionnaire.
+     * @param x  - La position en largeur.
+     * @param y  - La position en hauteur.
+     * @param gris - Le niveau de gris (NiveauGris)
+     */
     @Override
     public void definirPoint(int x, int y, NiveauGris gris) {
         CoupleObj <Integer,Integer> coupleObj = new CoupleObj<>(x, y);
@@ -64,6 +110,11 @@ public class ImageDict implements ImageGrise {
         }
     }
 
+    /**
+     * Définie le niveau de gris sur NOIR pour une position dans l'image.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     */
     @Override
     public void allumer(int x, int y) {
         if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
@@ -71,24 +122,38 @@ public class ImageDict implements ImageGrise {
         }
     }
 
+    /**
+     * Définie le niveau de gris à BLANC pour une position dans l'image.
+     * Dans ce cas, supprime le niveau de gris à cette position.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     */
     @Override
     public void eteindre(int x, int y) {
         dictionnaire.enleverPour(new CoupleObj<>(x,y));
     }
 
+    /**
+     * Définie un niveau de gris aléatoire pour chaque position dans l'image.
+     * N'ajoute rien pour un niveau de gris BLANC.
+     */
     @Override
     public void randomize() {
         for(int i = 0; i < largeur; i++){
             for (int j = 0; j < hauteur;j++){
-                NiveauGris niveauGris = NiveauGris.BLANC;
-                while(niveauGris.estBlanc()){
-                    niveauGris = NiveauGris.randomize();
+                NiveauGris niveauGris = NiveauGris.randomize();
+                if(!niveauGris.estBlanc()) {
+                    dictionnaire.ajouter(new CoupleObj<>(i, j), niveauGris);
                 }
-                dictionnaire.ajouter(new CoupleObj<>(i, j),niveauGris);
             }
         }
     }
 
+    /**
+     * Compte le nombre de NiveauGris présent dans l'image.
+     * @param gris - Le niveau de gris (NiveauGris)
+     * @return - Le nombre de points possedant le niveau de gris donné en parametre.
+     */
     @Override
     public int compterPoints(NiveauGris gris) {
         int nombre = 0;
@@ -104,6 +169,10 @@ public class ImageDict implements ImageGrise {
         return nombre;
     }
 
+    /**
+     * Donne une nouvelle image avec pour chaque point de l'image son niveau de gris inverse.
+     * @return - Une nouvelle image contenant l'inverse des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise inverser() {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -121,6 +190,10 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image avec pour chaque point de l'image son niveau de gris plus clair.
+     * @return Une nouvelle image contenant une nuance plus claire des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise eclaircir() {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -136,6 +209,10 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     Donne une nouvelle image avec pour chaque point de l'image son niveau de gris plus foncé.
+     * @return Une nouvelle image contenant une nuance plus foncée des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise assombrir() {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -156,6 +233,10 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Duplique l'image courante.
+     * @return Une nouvelle image identique à l'image courante.
+     */
     @Override
     public ImageGrise dupliquer() {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -172,6 +253,11 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est l'addition de chaque point de l'image donnée en parametre avec l'image courante.
+     * @param img - L'image (ImageGrise) à ajouter à l'image courante.
+     * @return - Une nouvelle image qui est l'addition de chaque point de l'image donnée en parametre avec l'image courante
+     */
     @Override
     public ImageGrise ajouter(ImageGrise img) {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -191,6 +277,11 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est la soustraction de chaque point de l'image donnée en parametre à l'image courante.
+     * @param img - L'image (ImageGrise) à soustraire à l'image courante.
+     * @return - Une nouvelle image qui est la soustraction de chaque point de l'image donnée en parametre à l'image courante
+     */
     @Override
     public ImageGrise soustraire(ImageGrise img) {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -200,7 +291,7 @@ public class ImageDict implements ImageGrise {
                 for (int j = 0; j < hauteur(); j++) {
                     NiveauGris niveauGris = dictionnaire.valeurPour(new CoupleObj<>(i,j));
                     if(niveauGris != null) {
-                        imageGrise.definirPoint(i, j, img.pointEn(i, j).soustraire(niveauGris));
+                        imageGrise.definirPoint(i, j, niveauGris.soustraire(img.pointEn(i, j)));
                     }
                 }
             }
@@ -208,6 +299,11 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est le résultat du XOR de chaque point de l'image donnée en parametre avec l'image courante.
+     * @param img - L'image (ImageGrise) avec laquelle on applique le XOR à l'image courante.
+     * @return - Une nouvelle image qui est le résultat du XOR de chaque point de l'image donnée en parametre à l'image courante
+     */
     @Override
     public ImageGrise XOR(ImageGrise img) {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -227,6 +323,11 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image dont chaque point est présent à la fois dans l'image donnée en parametre et dans l'image courante pour la même position.
+     * @param img - L'image (ImageGrise) avec laquelle faire l'intersection de l'image courante.
+     * @return - Une nouvelle image dont chaque point est présent à la fois dans l'image donnée en parametre et dans l'image courante pour la même position.
+     */
     @Override
     public ImageGrise intersection(ImageGrise img) {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -245,6 +346,10 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne le niveau moyen de gris dans l'image courante.
+     * @return - Le niveau moyen de gris dans l'image courante.
+     */
     @Override
     public NiveauGris niveauMoyen() {
 
@@ -264,6 +369,11 @@ public class ImageDict implements ImageGrise {
 
     }
 
+    /**
+     * Donne une nouvelle image dans laquelle le contraste est augmenté pour chaque point de l'image.
+     * On eclaircit les points clairs et on assombrit les points sombre.
+     * @return - Une nouvelle image dont le contraste est augmenté.
+     */
     @Override
     public ImageGrise augmenterContraste() {
         ImageGrise imageGrise = new ImageDict(largeur(), hauteur());
@@ -289,6 +399,12 @@ public class ImageDict implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une image dans laquelle on optimise l'utilisation de la mémoire selon le nombre de point BLANC.
+     * Car une ImageDict ne contient aucun BLANC. Ainsi, une image contenant uniquement des blancs
+     * sera chargée plus rapidement si c'est une ImageDict car le dictionnaire sera vide.
+     * @return - Une ImageTab si l'image ne contient pas beaucoup de BLANC et une ImageDict si la proportion de BLANC est importante.
+     */
     @Override
     public ImageGrise optimiser() {
 
@@ -308,6 +424,7 @@ public class ImageDict implements ImageGrise {
                     }
                 }
             }
+            return imageTab;
         }
         return this;
     }
--- Projet4(Niveaux de Gris)/sources/image/ImageTab.java
+++ Projet4(Niveaux de Gris)/sources/image/ImageTab.java
@@ -1,9 +1,25 @@
 package image;
 
+/**
+ * Classe représentant une image en niveaux de gris avec un tableau à deux dimensions de NiveauGris
+ * contenant un niveau de gris dans chaques cases.
+ * La premiere dimension représente la largeur de l'image.
+ * La deuxieme dimension représente la hauteur de l'image.
+ */
 public class ImageTab implements ImageGrise {
 
+    /**
+     *Le tableau contenant les niveaux de gris.
+     */
     private NiveauGris[][] niveauGris;
 
+    /**
+     /**
+     * Le constructeur de l'ImageDict. Avec en parametre la largeur et la hauteur de l'image.
+     * L'image est initialisé avec des niveaux de gris BLANC pour chaque point de l'image.
+     * @param largeur - La largeur de l'image.
+     * @param hauteur - La hauteur de l'image.
+     */
     public ImageTab(int largeur, int hauteur) {
 
         niveauGris = new NiveauGris[largeur][hauteur];
@@ -17,16 +33,30 @@ public class ImageTab implements ImageGrise {
         }
     }
 
+    /**
+     * Donne la largeur de l'image.
+     * @return la largeur de l'image.
+     */
     @Override
     public int largeur() {
         return niveauGris.length;
     }
 
+    /**
+     * La hauteur de l'image.
+     * @return - La hauteur de l'image.
+     */
     @Override
     public int hauteur() {
         return niveauGris[0].length;
     }
 
+    /**
+     * Donne le niveau de gris d'une position dans l'image donnée en parametre.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     * @return Le NiveauGris à la position donnée.
+     */
     @Override
     public NiveauGris pointEn(int x, int y) {
 
@@ -36,6 +66,12 @@ public class ImageTab implements ImageGrise {
         return null;
     }
 
+    /**
+     * Définie le niveau de gris (NiveauGris) sur une position dans l'image.
+     * @param x  - La position en largeur.
+     * @param y  - La position en hauteur.
+     * @param gris - Le niveau de gris (NiveauGris)
+     */
     @Override
     public void definirPoint(int x, int y, NiveauGris gris) {
         if (x >= 0 && x < largeur() && y >= 0 && y < hauteur()) {
@@ -43,6 +79,11 @@ public class ImageTab implements ImageGrise {
         }
     }
 
+    /**
+     * Définie le niveau de gris sur NOIR pour une position dans l'image.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     */
     @Override
     public void allumer(int x, int y) {
         if (x >= 0 && x < largeur() && y >= 0 && y < hauteur()) {
@@ -50,6 +91,11 @@ public class ImageTab implements ImageGrise {
         }
     }
 
+    /**
+     * Définie le niveau de gris à BLANC pour une position dans l'image.
+     * @param x - La position en largeur.
+     * @param y - La position en hauteur.
+     */
     @Override
     public void eteindre(int x, int y) {
         if (x >= 0 && x < largeur() && y >= 0 && y < hauteur()) {
@@ -57,6 +103,9 @@ public class ImageTab implements ImageGrise {
         }
     }
 
+    /**
+     * Définie un niveau de gris aléatoire pour chaque position dans l'image.
+     */
     @Override
     public void randomize() {
 
@@ -67,6 +116,11 @@ public class ImageTab implements ImageGrise {
         }
     }
 
+    /**
+     * Compte le nombre de NiveauGris présent dans l'image.
+     * @param gris - Le niveau de gris (NiveauGris)
+     * @return - Le nombre de points possedant le niveau de gris donné en parametre.
+     */
     @Override
     public int compterPoints(NiveauGris gris) {
         int nbDePoint = 0;
@@ -81,6 +135,10 @@ public class ImageTab implements ImageGrise {
         return nbDePoint;
     }
 
+    /**
+     * Donne une nouvelle image avec pour chaque point de l'image son niveau de gris inverse.
+     * @return - Une nouvelle image contenant l'inverse des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise inverser() {
 
@@ -94,6 +152,10 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image avec pour chaque point de l'image son niveau de gris plus clair.
+     * @return Une nouvelle image contenant une nuance plus claire des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise eclaircir() {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -108,6 +170,10 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     Donne une nouvelle image avec pour chaque point de l'image son niveau de gris plus foncé.
+     * @return Une nouvelle image contenant une nuance plus foncée des niveaux de gris présents dans l'image courante.
+     */
     @Override
     public ImageGrise assombrir() {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -123,6 +189,10 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Duplique l'image courante.
+     * @return Une nouvelle image identique à l'image courante.
+     */
     @Override
     public ImageGrise dupliquer() {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -136,6 +206,11 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est l'addition de chaque point de l'image donnée en parametre avec l'image courante.
+     * @param img - L'image (ImageGrise) à ajouter à l'image courante.
+     * @return - Une nouvelle image qui est l'addition de chaque point de l'image donnée en parametre avec l'image courante
+     */
     @Override
     public ImageGrise ajouter(ImageGrise img) {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -150,6 +225,11 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est la soustraction de chaque point de l'image donnée en parametre à l'image courante.
+     * @param img - L'image (ImageGrise) à soustraire à l'image courante.
+     * @return - Une nouvelle image qui est la soustraction de chaque point de l'image donnée en parametre à l'image courante
+     */
     @Override
     public ImageGrise soustraire(ImageGrise img) {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -164,6 +244,11 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image qui est le résultat du XOR de chaque point de l'image donnée en parametre avec l'image courante.
+     * @param img - L'image (ImageGrise) avec laquelle on applique le XOR à l'image courante.
+     * @return - Une nouvelle image qui est le résultat du XOR de chaque point de l'image donnée en parametre à l'image courante
+     */
     @Override
     public ImageGrise XOR(ImageGrise img) {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -178,6 +263,11 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une nouvelle image dont chaque point est présent à la fois dans l'image donnée en parametre et dans l'image courante pour la même position.
+     * @param img - L'image (ImageGrise) avec laquelle faire l'intersection de l'image courante.
+     * @return - Une nouvelle image dont chaque point est présent à la fois dans l'image donnée en parametre et dans l'image courante pour la même position.
+     */
     @Override
     public ImageGrise intersection(ImageGrise img) {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -195,6 +285,10 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne le niveau moyen de gris dans l'image courante.
+     * @return - Le niveau moyen de gris dans l'image courante.
+     */
     @Override
     public NiveauGris niveauMoyen() {
 
@@ -214,6 +308,11 @@ public class ImageTab implements ImageGrise {
 
     }
 
+    /**
+     * Donne une nouvelle image dans laquelle le contraste est augmenté pour chaque point de l'image.
+     * On eclaircit les points clairs et on assombrit les points sombre.
+     * @return - Une nouvelle image dont le contraste est augmenté.
+     */
     @Override
     public ImageGrise augmenterContraste() {
         ImageGrise imageGrise = new ImageTab(largeur(), hauteur());
@@ -236,6 +335,12 @@ public class ImageTab implements ImageGrise {
         return imageGrise;
     }
 
+    /**
+     * Donne une image dans laquelle on optimise l'utilisation de la mémoire selon le nombre de point BLANC.
+     * Car une ImageDict ne contient aucun BLANC. Ainsi, une image contenant uniquement des blancs
+     * sera chargée plus rapidement si c'est une ImageDict car le dictionnaire sera vide.
+     * @return - Une ImageTab si l'image ne contient pas beaucoup de BLANC et une ImageDict si la proportion de BLANC est importante.
+     */
     @Override
     public ImageGrise optimiser() {
         double nbDeBlancs = compterPoints(NiveauGris.BLANC);
@@ -248,6 +353,7 @@ public class ImageTab implements ImageGrise {
                      imageDict.definirPoint(i,j,this.pointEn(i,j));
                  }
              }
+             return imageDict;
         }
         return this;
     }
